bed2e624e14ffef59ab6f35a1814b698b83319d6
Matthew Works <mworks4905@gmail.com>
2026-03-05 22:25:56 -0700

    update func and test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Matt")
-	want := "Hello, Matt"
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello("Mat")
+		want := "Hello, Matt"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got , want)
 	}
